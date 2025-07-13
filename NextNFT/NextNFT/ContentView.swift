e9b953ff843ee80a28e62c7b0da9c7afe4a594dd
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-13 19:20:53 +0400

    first commit

---
+++ NextNFT/NextNFT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextNFT
+//
+//  Created by Nino Kurshavishvili on 13.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextNFT/NextNFT/NextNFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextNFTApp.swift
+//  NextNFT
+//
+//  Created by Nino Kurshavishvili on 13.07.25.
+//
+
+import SwiftUI
+
+@main
+struct NextNFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
