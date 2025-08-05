98ef789717c49cf6dac6732a8e911e512da8b352
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-08-05 18:35:49 +0400

    Add Collection Items Collected

--- NextNFT/NextNFT/Home/Views/TrendingCollection/CollectionContainerView.swift
+++ NextNFT/NextNFT/Home/Views/TrendingCollection/CollectionContainerView.swift
@@ -9,6 +9,26 @@ import SwiftUI
 //TODO: here goes full container UI
 struct CollectionContainerView: View {
     var body: some View {
-        
+        VStack {
+            HStack {
+                CollectionProfileView()
+                
+                Spacer()
+                
+                Image(systemName: "arrow.forward")
+                    .foregroundColor(.white)
+                    .font(.system(size: 16, weight: .medium))
+                    .padding()
+                    .background(AppColors.lightGrey.opacity(0.3))
+            }
+            .padding()
+        }
     }
+}
+
+
+
+#Preview {
+    CollectionContainerView()
+        .background(AppColors.darkBackground)
 }
