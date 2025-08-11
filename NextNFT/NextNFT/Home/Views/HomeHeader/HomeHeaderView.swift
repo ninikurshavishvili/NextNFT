ea102c6b58d1805398181af50aa4b41493e9239c
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-08-12 01:00:16 +0400

    Add Header View Files

---
+++ NextNFT/NextNFT/Home/Views/HomeHeader/HomeHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeHeaderView.swift
+//  NextNFT
+//
+//  Created by Nino Kurshavishvili on 12.08.25.
+//
+
+import SwiftUI
+
+struct HomeHeaderView {
+    var body: some View {
+        Text("Header for HOmepage")
+    }
+}
--- NextNFT/NextNFT/Home/Views/TrendingCollection/CollectionProfileView.swift
+++ NextNFT/NextNFT/Home/Views/TrendingCollection/CollectionProfileView.swift
@@ -25,7 +25,6 @@ struct CollectionProfileView : View {
         }
     }
 }
-//haaa
 
 #Preview {
     CollectionProfileView()
