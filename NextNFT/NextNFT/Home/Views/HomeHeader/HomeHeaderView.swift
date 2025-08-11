3464f97cec88c76c307a2639590d0f4e7032cdbc
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-08-12 01:25:30 +0400

    Add Header View Updates

--- NextNFT/NextNFT/Home/Views/HomeHeader/HomeHeaderView.swift
+++ NextNFT/NextNFT/Home/Views/HomeHeader/HomeHeaderView.swift
@@ -7,21 +7,36 @@
 
 import SwiftUI
 
+
 struct HomeHeaderView: View {
     var body: some View {
-        
-        Text("Next")
-            .foregroundStyle(.darkBackground)
+        HStack {
+            (
+                Text("Next")
+                    .foregroundStyle(.white)
+                +
+                Text("NFT")
+                    .foregroundStyle(.mainPurple)
+            )
             .font(.system(.title2, design: .serif, weight: .bold))
-
-        +
-        Text("NFT")
-            .foregroundStyle(.mainPurple)
-            .font(.system(.title2, design: .serif, weight: .bold))
-
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image(systemName: "line.3.horizontal")
+                    .font(.system(size: 20, weight: .medium))
+                    .foregroundStyle(.white)
+                    .frame(width: 44, height: 44)
+                    .background(AppColors.darkBackground.opacity(0.9))
+                    .clipShape(Circle())
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, 8)
+        .background(Color.black) // matches screenshot background
     }
-
 }
+
 
 
 #Preview {
