9aa0252859c7a68b02e891a026e4b5456c8d36a6
Nini Kurshavishvili <ninikurshavishvili@Ninis-MacBook-Air.local>
2026-01-27 05:35:26 -0800

    Added Title and info View Elements

--- NextNFT/NextNFT/Features/Home/Presentation/Views/CustomNFT/CustomNFTCardView.swift
+++ NextNFT/NextNFT/Features/Home/Presentation/Views/CustomNFT/CustomNFTCardView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CustomNFTCardView: View {
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Image("image")
                 .resizable()
                 .scaledToFit()
@@ -18,8 +18,19 @@ struct CustomNFTCardView: View {
                 .cornerRadius(10)
                 .padding()
 
-            Text("Custom NFT")
-                .font(.title)
+            VStack(alignment: .leading) {
+                Text("CryptoPunks V1")
+                    .font(.headline)
+                    .padding(.horizontal)
+                    .foregroundStyle(Color(.systemGray2))
+                Text("""
+                    Released on June 9, 2017 by Larva Labs. This is the original ERC-20 token now tradable via a modern ERC-721 wrapper.
+                    """)
+                .font(.footnote .bold())
+                .foregroundStyle(.lightGrey)
+                .padding(.horizontal)
+                
+            }
         }
     }
 }
