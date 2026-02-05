02d560190479d68813452121da057ba1cdf39130
Nini Kurshavishvili <ninikurshavishvili@Ninis-MacBook-Air.local>
2026-02-05 16:53:32 +0400

    Added getDomainColor

--- NextNFT/NextNFT/Extensions/Helpers/ImageColorAnalyzer.swift
+++ NextNFT/NextNFT/Extensions/Helpers/ImageColorAnalyzer.swift
@@ -11,6 +11,15 @@ import CoreImage.CIFilterBuiltins
 
 class ImageColorAnalyzer {
     
+    static func getDomainColor(from image: UIImage) -> Color? {
+        guard let inputImage = CIImage(image: image) else {return nil}
+        
+        // Set up the filter
+               let filter = CIFilter.areaAverage()
+               filter.inputImage = inputImage
+               filter.extent = inputImage.extent
+        
+    }
 }
 
 
